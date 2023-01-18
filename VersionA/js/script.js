808e3da0e5012aeaf2f6f7875bb1c90cc59106de
Danidan8 <104461462+Danidan8@users.noreply.github.com>
2023-01-18 11:41:17 +0100

    VersionA finished

--- VersionA/js/script.js
+++ VersionA/js/script.js
@@ -1,4 +1,6 @@
 $('.x-button').click(removeOverlay);
+$('.send-button').click(removeOverlay);
+// $('html').keypress(removeOverlay);
 
 function removeOverlay(e) { 
     e.preventDefault();
@@ -7,11 +9,37 @@ function removeOverlay(e) {
 }
 
 
-$('.available-item>img').click(addOverlay);
+// $('.available-item>img').click(addOverlay);
 
-function addOverlay(e) { 
+// function addOverlay(e) { 
+//     e.preventDefault();
+//     $('.overlay').css('display', 'flex');
+// }
+
+$('.rentable-item>img').click(OverlayToRentable);
+function OverlayToRentable(e){
     e.preventDefault();
+    $('.contact-owner-button').css('visibility', 'visible');
     $('.overlay').css('display', 'flex');
-    // console.log('I am being trigered');
-    // $('section').css('background-color', 'red');
+    $('form.overlay-form').css('visibility', 'hidden');
+}
+
+$('.lent-item>img').click(OverlayToLent);
+function OverlayToLent(e){
+    e.preventDefault();
+    $('.contact-owner-button').css('visibility', 'hidden');
+    $('.overlay').css('display', 'flex');
+    $('form.overlay-form').css('display', 'none');
+}
+
+$('.contact-owner-button').click(openMessageSender);
+function openMessageSender(e) { 
+    e.preventDefault();
+    $('form.overlay-form').css('visibility', 'visible');   
+}
+
+$('.back-arrow').click(backArrow);
+function backArrow(e) { 
+    e.preventDefault();
+    $('form.overlay-form').css('visibility', 'hidden');   
 }
