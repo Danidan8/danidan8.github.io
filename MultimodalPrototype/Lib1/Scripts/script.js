0e758912b5c4a482474210673c82546d204fe751
Danidan8 <104461462+Danidan8@users.noreply.github.com>
2023-02-20 14:58:02 +0100

    ::before

--- MultimodalPrototype/Lib1/Scripts/script.js
+++ MultimodalPrototype/Lib1/Scripts/script.js
@@ -3,8 +3,8 @@ var gn = new GyroNorm();
 gn.init().then(function(){
   gn.start(function(data){
   
-    $('.output1').html(data.dm.x).before("data.dm.x: ");
-    $('.output2').html(data.do.alpha).before("data.do.alpha: ");
+    $('.output1').html(data.dm.x);
+    $('.output2').html(data.do.alpha);
 
 });
 }).catch(function(e){
