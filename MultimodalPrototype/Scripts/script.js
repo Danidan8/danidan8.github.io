722937370c15485cc48affa46668f1741b784af6
Danidan8 <104461462+Danidan8@users.noreply.github.com>
2023-02-20 16:01:08 +0100

    Libs

---
+++ MultimodalPrototype/Scripts/script.js
@@ -0,0 +1,50 @@
+var gn = new GyroNorm();
+
+gn.init().then(function(){
+  gn.start(function(data){
+  
+    $('.output1').html(data.dm.x);
+    $('.output2').html(data.do.alpha);
+
+});
+}).catch(function(e){
+    console.log("Not Supported")
+  });
+
+//-------------------------------------------------
+
+let y = 0;
+let colorR = 153;
+let colorG = 153;
+let colotB = 153;
+
+let speach = new p5.SpeechRec('en-US', parseResult);
+speach.continuous = true;
+
+function setup(){
+  createCanvas(100, 100);
+  // speach.onResult = parseResult;
+  speach.start();
+}
+
+function draw(){
+  background(colorR, colorG, colotB);
+  ellipse(50, y, 10, 10);
+  y+=1;
+  if (y>height){
+    y = 0;
+  }
+}
+
+function parseResult(){
+  // if(speach.resultValue==true){
+  //   $('.output3').html(speach.resultString);
+  //   console.log(speach.resultString);
+  // }
+  var mostRecentWord = speach.resultString.split(' ').pop().toLowerCase();
+  console.log(mostRecentWord);
+  if(mostRecentWord.indexOf("yellow")!==-1){colorR=238; colorG = 255; colotB = 0;}
+  else if(mostRecentWord.indexOf("red")!==-1){colorR=255; colorG = 0; colotB = 13;}
+  else if(mostRecentWord.indexOf("grey")!==-1){colorR=153; colorG = 153; colotB = 153;}
+  else if(mostRecentWord.indexOf("purple")!==-1){colorR=255; colorG = 0; colotB = 221;}
+}
