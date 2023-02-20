ae9d1e9e21a7ce95bbeafb0ae1e0e774685ad465
Danidan8 <104461462+Danidan8@users.noreply.github.com>
2023-02-20 14:56:31 +0100

    Added a second variable

--- MultimodalPrototype/Lib1/Scripts/script.js
+++ MultimodalPrototype/Lib1/Scripts/script.js
@@ -3,7 +3,9 @@ var gn = new GyroNorm();
 gn.init().then(function(){
   gn.start(function(data){
   
-    $('.output').html(data.dm.x);
+    $('.output1').html(data.dm.x).before("data.dm.x: ");
+    $('.output2').html(data.do.alpha).before("data.do.alpha: ");
+
 });
 }).catch(function(e){
     console.log("Not Supported")
