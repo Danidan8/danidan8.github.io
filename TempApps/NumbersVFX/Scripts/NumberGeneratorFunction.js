2a94063a788bea3ed0af895648e04bfdc7dd17c7
Danidan8 <104461462+Danidan8@users.noreply.github.com>
2023-10-04 11:32:55 +0200

    Added NumbersVFX - TempApp

---
+++ TempApps/NumbersVFX/Scripts/NumberGeneratorFunction.js
@@ -0,0 +1,160 @@
+function generateNum(thousands, hundreds, tens, ones) {
+    var xPos = 250;
+    var yPos = 250;
+
+    stroke(0);
+    strokeWeight(10);
+    line(xPos, yPos - 150, xPos, yPos + 150);
+    
+    switch(ones) {
+    default:
+    case 0:
+      break;
+    case 1:
+      line(xPos, yPos - 150, xPos + 100, yPos - 150);
+      break;
+    case 2:
+      line(xPos, yPos - 50, xPos + 100, yPos - 50);
+      break;
+    case 3:
+      line(xPos, yPos - 150, xPos + 100, yPos - 50);
+      break;
+    case 4:
+      line(xPos, yPos - 50, xPos + 100, yPos - 150);
+      break;
+    case 5:
+      line(xPos, yPos - 150, xPos + 100, yPos - 150);
+      line(xPos, yPos - 50, xPos + 100, yPos - 150);
+      break;
+    case 6:
+      line(xPos + 100, yPos - 150, xPos + 100, yPos - 50);
+      break;
+    case 7:
+      line(xPos, yPos - 150, xPos + 100, yPos - 150);
+      line(xPos + 100, yPos - 150, xPos + 100, yPos - 50);
+      break;
+    case 8:
+      line(xPos + 100, yPos - 150, xPos + 100, yPos - 50);
+      line(xPos, yPos - 50, xPos + 100, yPos - 50);
+      break;
+    case 9:
+      line(xPos, yPos - 150, xPos + 100, yPos - 150);
+      line(xPos + 100, yPos - 150, xPos + 100, yPos - 50);
+      line(xPos, yPos - 50, xPos + 100, yPos - 50);
+      break;
+    }
+  
+    switch(tens) {
+    default:
+    case 0:
+      break;
+    case 1:
+      line(xPos, yPos - 150, xPos - 100, yPos - 150);
+      break;
+    case 2:
+      line(xPos, yPos - 50, xPos - 100, yPos - 50);
+      break;
+    case 3:
+      line(xPos, yPos - 150, xPos - 100, yPos - 50);
+      break;
+    case 4:
+      line(xPos, yPos - 50, xPos - 100, yPos - 150);
+      break;
+    case 5:
+      line(xPos, yPos - 150, xPos - 100, yPos - 150);
+      line(xPos, yPos - 50, xPos - 100, yPos - 150);
+      break;
+    case 6:
+      line(xPos - 100, yPos - 150, xPos - 100, yPos - 50);
+      break;
+    case 7:
+      line(xPos, yPos - 150, xPos - 100, yPos - 150);
+      line(xPos - 100, yPos - 150, xPos - 100, yPos - 50);
+      break;
+    case 8:
+      line(xPos, yPos - 50, xPos - 100, yPos - 50);
+      line(xPos - 100, yPos - 150, xPos - 100, yPos - 50);
+      break;
+    case 9:
+      line(xPos, yPos - 150, xPos - 100, yPos - 150);
+      line(xPos, yPos - 50, xPos - 100, yPos - 50);
+      line(xPos - 100, yPos - 150, xPos - 100, yPos - 50);
+      break;
+    }
+  
+    switch(hundreds) {
+    default:
+    case 0:
+      break;
+    case 1:
+      line(xPos, yPos + 150, xPos + 100, yPos + 150 );
+      break;
+    case 2:
+      line(xPos, yPos + 50, xPos + 100, yPos + 50 );
+      break;
+    case 3:
+      line(xPos, yPos + 150, xPos + 100, yPos + 50 );
+      break;
+    case 4:
+      line(xPos, yPos + 50, xPos + 100, yPos + 150 );
+      break;
+    case 5:
+      line(xPos, yPos + 150, xPos + 100, yPos + 150 );
+      line(xPos, yPos + 50, xPos + 100, yPos + 150 );
+      break;
+    case 6:
+      line(xPos + 100, yPos + 150, xPos + 100, yPos + 50 );
+      break;
+    case 7:
+      line(xPos, yPos + 150, xPos + 100, yPos + 150 );
+      line(xPos + 100, yPos + 150, xPos + 100, yPos + 50 );
+      break;
+    case 8:
+      line(xPos + 100, yPos + 150, xPos + 100, yPos + 50 );
+      line(xPos, yPos + 50, xPos + 100, yPos + 50 );
+      break;
+    case 9:
+      line(xPos, yPos + 150, xPos + 100, yPos + 150 );
+      line(xPos + 100, yPos + 150, xPos + 100, yPos + 50 );
+      line(xPos, yPos + 50, xPos + 100, yPos + 50 );
+      break;
+    }
+  
+    switch(thousands) {
+    default:
+    case 0:
+      break;
+    case 1:
+      line(xPos, yPos + 150, xPos - 100, yPos + 150 );
+      break;
+    case 2:
+      line(xPos, yPos + 50, xPos - 100, yPos + 50 );
+      break;
+    case 3:
+      line(xPos, yPos + 150, xPos - 100, yPos + 50 );
+      break;
+    case 4:
+      line(xPos, yPos + 50, xPos - 100, yPos + 150 );
+      break;
+    case 5:
+      line(xPos, yPos + 150, xPos - 100, yPos + 150 );
+      line(xPos, yPos + 50, xPos - 100, yPos + 150 );
+      break;
+    case 6:
+      line(xPos - 100, yPos + 50, xPos - 100, yPos + 150 );
+      break;
+    case 7:
+      line(xPos, yPos + 150, xPos - 100, yPos + 150 );
+      line(xPos - 100, yPos + 50, xPos - 100, yPos + 150 );
+      break;
+    case 8:
+      line(xPos, yPos + 50, xPos - 100, yPos + 50 );
+      line(xPos - 100, yPos + 50, xPos - 100, yPos + 150 );
+      break;
+    case 9:
+      line(xPos, yPos + 50, xPos - 100, yPos + 50 );
+      line(xPos, yPos + 150, xPos - 100, yPos + 150 );
+      line(xPos - 100, yPos + 50, xPos - 100, yPos + 150 );
+      break;
+    }
+  }
---
+++ TempApps/NumbersVFX/Scripts/script.js
@@ -0,0 +1,39 @@
+
+
+function setup(){
+    createCanvas(500, 500);
+    
+    generateNum(1,2,3,4)
+}
+
+$('#RandomizeButton').click(function (e) { 
+    e.preventDefault();
+    clear();
+    generateRandomNum();
+});
+
+
+function generateRandomNum() {
+    var firstDgit = Math.floor(Math.random() * 9) 
+    var secondDgit = Math.floor(Math.random() * 9) 
+    var thirdDgit = Math.floor(Math.random() * 9) 
+    var fourthDgit = Math.floor(Math.random() * 9) 
+    generateNum(firstDgit, secondDgit,thirdDgit,fourthDgit);
+    $('#NumInput').val("" + firstDgit + secondDgit + thirdDgit + fourthDgit);
+    
+}
+
+$('#SubmitButton').click(function (e) { 
+    e.preventDefault();
+    var inputNum = $('#NumInput').val();
+    if (inputNum && inputNum <= 9999 && inputNum >= 0){
+        clear();
+        generateNum(parseInt(inputNum[inputNum.length - 4]),parseInt(inputNum[inputNum.length - 3]),parseInt(inputNum[inputNum.length - 2]),parseInt(inputNum[inputNum.length - 1]))
+    }else{
+        clear();
+        textAlign(CENTER)
+        textSize(40)
+        noStroke()
+        text("Unexpected Input", 250, 400)
+    }
+});
