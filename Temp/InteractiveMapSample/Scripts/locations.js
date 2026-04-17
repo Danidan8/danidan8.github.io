719e80656ac62bd357b46824018b01a857198f05
Danidan <104461462+Danidan8@users.noreply.github.com>
2026-04-17 19:55:59 +0300

    Created Mapbox Demo

---
+++ Temp/InteractiveMapSample/Scripts/locations.js
@@ -0,0 +1,30 @@
+
+const chapters = [
+    {
+      id: "telAviv",
+      location: {
+        center: [34.77585, 32.09647],
+        zoom: 15,
+        pitch: 30,
+        bearing: 0
+      }
+    },
+    {
+      id: "gaza",
+      location: {
+        center: [34.45918, 31.51267],
+        zoom: 13,
+        pitch: 45,
+        bearing: 20
+      }
+    },
+    {
+      id: "portSaid",
+      location: {
+        center: [32.31962, 31.22315],
+        zoom: 13,
+        pitch: 25,
+        bearing: 0
+      }
+    }
+  ];
